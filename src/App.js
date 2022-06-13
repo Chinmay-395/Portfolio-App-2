0de791e540ef15a6cf84e763e960a30fef9ad3b8
Chinmay395 <chinmaydali395@gmail.com>
2022-06-13 23:14:28 +0530

    All components in a single page

--- src/App.js
+++ src/App.js
@@ -36,13 +36,14 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/project" element={<Projects />} /> */}
+          {/* <Route path="/about" element={<About />} /> */}
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <About />
-        {/* <Footer /> */}
+        <Projects />
+        <Footer />
       </div>
     </Router>
   );
