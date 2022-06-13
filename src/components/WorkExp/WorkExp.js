2c3661e18b2f67ccfea252c865e00ddfe8879aba
Chinmay395 <chinmaydali395@gmail.com>
2022-06-13 23:39:37 +0530

    Fixed the linting issues

---
+++ src/components/WorkExp/WorkExp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import VerticalTimeline from "./VerticalTimeline";
+function WorkExp() {
+  return (
+    <Container fluid className="about-section">
+      <Container>
+        <VerticalTimeline />
+      </Container>
+    </Container>
+  );
+}
+
+export default WorkExp;
